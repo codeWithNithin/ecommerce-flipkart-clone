005d9025e60d7d333c65940a392fac4caa2a437b
codeWithNithin <nithinkottary25@gmail.com>
2025-09-21 14:51:05 +0530

    router implementation done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import ProductList from "./pages/product-list/ProductList";
+import ErrorPage from "./pages/Error/ErrorPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/product-list/:category" element={<ProductList />}></Route>
+      <Route path="*" element={<ErrorPage />}></Route>
     </Routes>
   );
 }
